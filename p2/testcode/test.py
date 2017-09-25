b1a19a344cb212bd6af9db7853b2dd653ff7c895
Zaibo Wang <zaibowang@gmail.com>
2017-09-25 15:22:42 -0400

    Circuit complete, all test cases administered

--- p2/testcode/test.py
+++ p2/testcode/test.py
@@ -1,33 +1,6 @@
 import random
 
-INSTRUCTION = [
-    # 'ADDIU',
-    # 'ANDI',
-    # 'ORI',
-    # 'XORI',
-    # 'SLTI',
-    # 'SLTIU',
-    # 'ADDU',
-    # 'SUBU',
-    # 'AND',
-    # 'OR',
-    # 'XOR',
-    # 'NOR',
-    # 'SLT',
-    # 'SLTU',
-    'MOVN',
-    'MOVZ',
-    # 'SLL', Did not test for hazards
-    # 'SRL', Did not test for hazards
-    # 'SRA', Did not test for hazards
-    # 'SLLV',
-    # 'SRLV',
-    # 'SRAV',
-    # 'LUI',
-]
-
-def get_instru():
-    return INSTRUCTION[random.randint(0,len(INSTRUCTION)-1)]
+f = open('generated_tests', 'w')
 
 def int_32():
     return random.randint(-2**15, 2**15-1)
@@ -65,9 +38,6 @@ def arith_immed():
         'SLTIU',
     ]
     return li[random.randint(0,1)]
-
-f = open('generated_tests', 'w')
-
 
 def ADD_IU(i):
     word = 'ADDIU' + ' $' + str(i) + ', ' + '$zero, ' + str(int_32()) + '\n'
@@ -146,20 +116,78 @@ def shift_var(i):
     ]
     instru = li[random.randint(0,2)]
     word = instru + ' $' + str(i) + ', ' + rand_regis() + ', ' + rand_regis() + '\n'
+    # word = instru + ' $' + str(i) + ', $' + str(i-1) + ', $' + str(i-2) + '\n'
+    # word = instru + ' $' + str(5) + ', $' + str(5) + ', $' + str(random.randint(1,6)) + '\n'
     f.write(word)
 
+def move(i):
+    li = ['MOVN', 'MOVZ']
+    instru = li[random.randint(0,1)]
+    word = instru + ' $' + str(i) + ', ' + rand_regis() + ', ' + rand_regis() + '\n'
+    f.write(word)
 
-for i in range(1,7):
+for i in range(1,32):
     ADD_IU(i)
 
-for i in range(7,32):
-    shift_var(i)
+load_store = [
+    'LB',
+    'LW',
+    'LBU',
+    'SB',
+    'SW',
+]
+def divis4int():
+    n = random.randint(0,5)
+    return n*32
+
+def memory():
+    instru = load_store[random.randint(0,4)]
+    word = instru + ' ' +  rand_regis_curr(31) + ', ' + str(divis4int()) + '(' + rand_regis_curr(31) + ')\n'
+    f.write(word)
+
+def jump():
+    # for J and JAL
+    # instru = jumps[random.randint(0,1)]
+    # word = instru + ' ' + str(random.randint(0,2**16)*4) + '\n'
+    
+    # JR
+    word = 'JR' + ' ' + rand_regis_curr(31) + '\n'
+    f.write(word)
+    # JALR
+    word = 'JALR' + ' ' + rand_regis_curr(31) + ', ' + rand_regis_curr(31) + '\n'
+    f.write(word)
+
+# BEQ BNE BLEZ BGTZ BLTZ BGEZ
+def branch():
+    # BEQ/BNE
+    # li = ['BEQ', 'BNE']
+    # word = li[random.randint(0,1)] + ' ' +  rand_regis_curr(31) + ', ' + rand_regis_curr(31) + ', ' + str(divis4int()) + '\n'
+    # f.write(word)
+    # BLTZ/BGTZ/BLTZ/BGEZ
+    others = ['BLTZ', 'BGTZ', 'BLTZ', 'BGEZ']
+    word = others[random.randint(0,3)] + ' ' + rand_regis_curr(31) + ', ' + str(divis4int()) + '\n'
+    f.write(word)
+
+for i in range(100):
+    # jump()
+    branch()
+
+# for i in range(7,32):
+    # shift_var(i)
     # shift(i)
     # logical(i)
     # arith_U_hazard(i)
     # logical_I()
     # SLT_test(i)
     # arith_U_rand(i)
+    # move(i)
+
+# def ALL_RANDOM():
+    # func_li = [shift_var, shift, logical, arith_U, logical_I, SLT_test, move, shift_var, LUI, arith_U]
+#     for i in range(1000):
+#         func = random.randint(0,9)
+#         regis = random.randint(0,31)
+#         func_li[func](regis)
 
 # for i in range(0, 32):
-#     LUI(i)
+#     move(i)
